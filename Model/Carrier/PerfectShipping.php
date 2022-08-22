1f586be4ec8ecec284f34bf659b1a83e0f29c796
Samuel Boczek <samuelboczek@gmail.com>
2022-08-22 17:22:30 +0100

    Initial Commit

---
+++ Model/Carrier/PerfectShipping.php
@@ -0,0 +1,100 @@
+<?php
+declare(strict_types=1);
+
+namespace PerfectShapes\PerfectShipping\Model\Carrier;
+
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Shipping\Model\Rate\Result;
+
+/**
+ * Perfect shipping calculates the price of shipping based on the destination country.
+ */
+class PerfectShipping extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
+    \Magento\Shipping\Model\Carrier\CarrierInterface
+{
+    /**
+     * @var string
+     */
+    protected $_code = 'perfectshipping';
+
+    /**
+     * @var \Magento\Shipping\Model\Rate\ResultFactory
+     */
+    protected $rateResultFactory;
+
+    /**
+     * @var \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory
+     */
+    protected $rateMethodFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory
+     * @param \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory,
+        \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory,
+        array $data = []
+    ) {
+        $this->rateResultFactory = $rateResultFactory;
+        $this->rateMethodFactory = $rateMethodFactory;
+        parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        $shippingPriceUk = $this->getConfigData('price_uk');
+        $shippingPriceAbroad = $this->getConfigData('price_abroad');
+
+        $result = $this->rateResultFactory->create();
+
+        if ($shippingPriceUk !== false && $shippingPriceAbroad !== false) {
+            $method = $this->rateMethodFactory->create();
+
+            $method->setCarrier($this->_code);
+            $method->setCarrierTitle($this->getConfigData('title'));
+
+            $method->setMethod($this->_code);
+            $method->setMethodTitle($this->getConfigData('name'));
+
+            if ($request->getFreeShipping() === true) {
+                $shippingPrice = '0.00';
+            } else {
+                $shippingPrice = $request->getDestCountryId() == 'GB' ? $shippingPriceUk : $shippingPriceAbroad;
+            }
+
+            $method->setPrice($shippingPrice);
+            $method->setCost($shippingPrice);
+
+            $result->append($method);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get allowed shipping methods.
+     *
+     * @return array
+     */
+    public function getAllowedMethods()
+    {
+        return [$this->_code => $this->getConfigData('name')];
+    }
+}
